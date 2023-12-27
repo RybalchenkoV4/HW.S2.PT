55c419e042c801955dc96d0bc4a8a76936e1a701
Vladimir Rybalchenko <rybalchenko.v4@gmail.com>
2023-12-27 21:37:33 +0300

    add task4

---
+++ task4/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+
+Console.WriteLine("Введите натуральное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string toStr = "";
+string revStr = "";
+
+if (n < 10)
+{
+    Console.Write(n);
+}
+else
+{
+    while (n > 0)
+    {
+        int x = n % 10;
+        n /= 10;
+        if (n > 0)
+        {
+            toStr = toStr + x.ToString() + ",";
+        }
+        else
+        {
+            toStr = toStr + x.ToString();
+        }
+    }
+}
+
+foreach (char i in toStr)
+{
+    revStr = i + revStr;
+}
+
+Console.WriteLine(revStr);
