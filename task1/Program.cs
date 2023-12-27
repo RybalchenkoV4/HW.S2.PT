e222a4cf09d9549bef1f833bd09aa67ebc48efa2
Vladimir Rybalchenko <rybalchenko.v4@gmail.com>
2023-12-27 19:29:31 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num%7 == 0 && num % 23 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
